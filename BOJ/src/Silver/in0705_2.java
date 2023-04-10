103886b6dae5b908344fc8f123a814ef2d728294
Seorim <seorima@naver.com>
2023-04-10 23:55:18 +0900

    inflearn

---
+++ BOJ/src/Silver/in0705_2.java
@@ -0,0 +1,19 @@
+package Silver;
+
+public class in0705_2 {
+	class Node{
+		int data;
+		Node lt,rt;
+		
+		public Node(int val) {
+			data = val;
+			lt=rt= null;
+		}	
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
