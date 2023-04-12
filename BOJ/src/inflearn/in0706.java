fa04a1bdf5be0b50f53bb58daccbd31ff4bd369c
Seorim <seorima@naver.com>
2023-04-12 23:53:14 +0900

    dfs

---
+++ BOJ/src/inflearn/in0706.java
@@ -0,0 +1,27 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class in0706 {
+	
+	public int DFS(int n) {
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		in0706 T = new in0706();
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		T.DFS(n);
+		
+		
+		
+
+	}
+
+}
