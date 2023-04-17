9336587340c624655e79dd9f6ce2aad89abef99d
seorima <51224706+seorima@users.noreply.github.com>
2023-04-17 23:35:23 +0900

    [Bronze II] Title: 영식이와 친구들, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.java
+++ 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.java
@@ -2,32 +2,47 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt(); //사람 수
-		int M = sc.nextInt(); //M번 받을 시 종료
-		int L = sc.nextInt(); //L만큼 돔
+    public static void main(String[] args) {
 
-		int people[] = new int [N+1];
-		int start = 1 , sum = 0;
-		while(true) {
-			
-			people[start] +=1;
-			if(people[start] == M)
-				break;
-			if(people[start]%2 == 0) {
-				//반시계 방향으로
-				start = (start-L)<=0?N-Math.abs(start-L):start-L;
-			}
-			else {
-				//시계방향으로
-				start = (start+L)>N?(start+L)%N:start+L;
-			}
-			
-			sum++;
-		}
-		
-		System.out.println(sum);
-	}
+        Scanner sc = new Scanner(System.in);
+
+        int[] array;
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int L = sc.nextInt();
+
+        array = new int[N + 1];
+        int count = 0;
+
+        int n = 1; // 공의 위치
+
+        while (true) {
+
+            array[n]++;
+
+            if (array[n] == M)
+                break;
+
+            if (array[n] % 2 == 1) {
+                n += L;
+
+                if (n > N) {
+                    n -= N;
+                }
+
+            } else {
+                n -= L;
+                if (n < 1) {
+                    n += N;
+                }
+            }
+
+            count++;
+
+        }
+
+        System.out.println(count);
+        sc.close();
+    }
 
 }
