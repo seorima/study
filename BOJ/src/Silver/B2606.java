553b6351b789de66464855e2e03a8cc32c6c1e33
Seorim <seorima@naver.com>
2023-04-24 18:13:28 +0900

    boj

---
+++ BOJ/src/Silver/B2606.java
@@ -0,0 +1,52 @@
+package Silver;
+
+import java.util.Scanner;
+
+public class B2606 {
+	
+	static int n=0;
+	static int m=0;
+	static int count =0;
+	static int[][] array;
+	static boolean[] check;
+	
+	public int DFS(int start ) {
+		check[start]=true;
+		
+		for(int i=1;i<=n;i++) {
+			if(array[start][i]==1 && check[i]==false) {
+
+				count++;
+				DFS(i);
+				
+			}
+		}
+		
+		return count;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		B2606 t = new B2606();
+		
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		array = new int[101][101];
+		check = new boolean[101];
+		
+		for(int i=1;i<=m;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			array[a][b] = array[b][a] = 1;
+		}
+		
+		System.out.println(t.DFS(1));
+		
+
+	}
+
+}
