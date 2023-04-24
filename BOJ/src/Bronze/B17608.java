5f0f616c1545122cfef929463057f92c3d5318ab
Seorim <seorima@naver.com>
2023-04-24 18:01:13 +0900

    boj

---
+++ BOJ/src/Bronze/B17608.java
@@ -0,0 +1,34 @@
+package Bronze;
+
+import java.util.Scanner;
+
+
+public class B17608 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int count = 1;
+		
+		int[] array = new int[N];
+		
+		for(int i=0;i<N;i++) {
+			array[i]=sc.nextInt();
+		}
+
+		int num = array[N-1];
+		
+		for(int i=N-1;i>=0;i--) {
+			if(num<array[i]) {
+				num=array[i];
+				count++;
+			}
+			
+		}
+		
+		System.out.println(count);
+	}
+
+}
