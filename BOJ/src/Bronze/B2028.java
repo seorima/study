cf39d5f85e0e9fe7dce9b37f36f02b3a5cd02a1c
Seorim <seorima@naver.com>
2023-04-24 18:12:44 +0900

    boj

---
+++ BOJ/src/Bronze/B2028.java
@@ -0,0 +1,29 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class B2028 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		
+		for(int i=0;i<t;i++) {
+			int n = sc.nextInt();
+			int num = n*n;
+			
+			int len = (int)n.length();
+			
+			System.out.println(len);
+			
+			
+			
+		}
+
+	}
+
+}
