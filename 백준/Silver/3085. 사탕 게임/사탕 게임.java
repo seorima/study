323cabce2e496afe435496c59a305b86a6076584
seorima <51224706+seorima@users.noreply.github.com>
2023-06-13 14:35:38 +0900

    [Silver II] Title: 사탕 게임, Time: 300 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class Main {
+    static int n;
+    static char[][] arr;
+
+    static int max ;
+
+
+    public static void swap(int x1,int x2,int y1,int y2){
+        char tmp = arr[x1][y1];
+        arr[x1][y1] = arr[x2][y2];
+        arr[x2][y2] = tmp;
+
+    }
+
+    public static void find(int row, int col){
+
+
+        for(int i=0;i<n;i++){
+            int cnt =1;
+            for(int j=0;j<n-1;j++){
+                if(arr[i][j] == (arr[i][j+1])){
+                    cnt++;
+                }else{
+                    cnt =1;
+                }
+                max = Math.max(max,cnt);
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            int cnt =1;
+            for(int j=0;j<n-1;j++){
+                if(arr[j][i] == (arr[j+1][i])){
+                    cnt++;
+
+                }else{
+                    cnt =1;
+                }
+                max = Math.max(max,cnt);
+            }
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        arr = new char[n][n];
+        max = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++) {
+            String str = sc.next();
+            for(int j=0; j<arr[i].length; j++) {
+                arr[i][j] = str.charAt(j);
+            }
+        }
+
+        for(int i=0;i<n;i++){ // 행
+            for(int j=0;j<n-1;j++){
+                    T.swap(i,i,j,j+1);
+                    T.find(i,j);
+                    T.swap(i,i,j+1,j);
+            }
+        }
+
+        for(int i=0;i<n;i++){ // 열
+            for(int j=0;j<n-1;j++){
+                    T.swap(j,j+1,i,i);
+                    T.find(j,i);
+                    T.swap(j+1,j,i,i);
+            }
+        }
+
+        System.out.println(max);
+
+
+
+
+    }
+}
