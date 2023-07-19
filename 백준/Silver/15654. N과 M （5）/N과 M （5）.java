5134d8ffcd17eeb05f981e576345f37f63b6fa9b
seorima <51224706+seorima@users.noreply.github.com>
2023-07-19 23:54:24 +0900

    [Silver III] Title: N과 M (5), Time: 2548 ms, Memory: 82648 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,43 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int n,m;
+    static int[] arr,result;
+    static int[] ch;
+    public static void Test(int L){
+        if(L==m){
+            for(int x : result){
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }else{
+            for(int i=0;i<n;i++){
+                if(ch[i]==0){
+                    ch[i]=1;
+                    result[L] = arr[i];
+                    Test(L+1);
+                    ch[i]=0;
+                }
+
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        m=sc.nextInt();
+
+        arr = new int[n];
+        result = new int[m];
+        ch = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        Test(0);
+    }
+}
