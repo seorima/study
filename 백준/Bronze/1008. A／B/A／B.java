15590101f6bf351964cda99e8b2aba0e17e1031f
seorima <51224706+seorima@users.noreply.github.com>
2023-09-27 22:41:49 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 17992 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,17 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        double n=sc.nextDouble();
+        double b = sc.nextDouble();
+        double result = n/b;
+        System.out.println(result);
+
+    }
+}
